cff511581f757c6ccbe8bafc915502062482cf82
niitagile <43024343+niitagile@users.noreply.github.com>
2022-04-25 12:43:28 +0530

    Add files via upload

---
+++ ChatServerProjet/index.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected!');
+  });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
